8d7e90c420b023c671b28475cbcb46a34d931dcc
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-12-15 12:44:33 -0600

    finished hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -35,7 +35,7 @@ const Header: FC = () => {
             <a href="/"><span className="text-xl font-bold uppercase">Luke&nbsp; Sorrenti</span></a>
             </div>
           <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full  inline-flex items-center justify-center">
+            <div className="size-11 border border-stone-400 rounded-full  inline-flex items-center justify-center bg-stone-200">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="3" y="15" width="18" height="2" fill="currentColor" />
                 <rect x="3" y="7" width="18" height="2" fill="currentColor" />
