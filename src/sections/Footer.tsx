1f4fae71032b3f2b90974ab1511179813fca90ad
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-12-30 09:38:02 -0600

    more styling for small screens on footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -35,46 +35,33 @@ const Footer: FC = () => {
         <span className="uppercase">One spot available for next nonth</span>
       </div>
       <h2 className="text-4xl mt-8 font-extralight">Enough talk let's make something great together</h2>
-      {/* <Button variant="secondary" 
-              className="mt-8"
-              iconAfter={
-        <svg 
-        xmlns="http://www.w3.org/2000/svg" fill="none" 
-        viewBox="0 0 24 24" 
-        strokeWidth="1.5" 
-        stroke="currentColor" 
-        className="size-6">
-        <path 
-        strokeLinecap="round" strokeLinejoin="round" 
-        d="m4.5 19.5 15-15m0 0H8.25m11.25 0v1125" />
-        </svg>
-        }>
-        <span className="p">info@test.com</span>
-        </Button> */}
-         <Button variant="secondary" iconAfter={
-     <svg 
-     xmlns="http://www.w3.org/2000/svg" fill="none" 
-     viewBox="0 0 24 24" 
-     strokeWidth="1.5" 
-     stroke="currentColor" 
-     className="size-5 mt-[0.6rem]">
-     <path 
-     strokeLinecap="round" strokeLinejoin="round" 
-     d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-     </svg>
+         <Button variant="secondary" 
+         className="mt-8"
+         iconAfter={
+              <svg 
+              xmlns="http://www.w3.org/2000/svg" fill="none" 
+              viewBox="0 0 24 24" 
+              strokeWidth="1.5" 
+              stroke="currentColor" 
+              className="size-5 mt-[0.7rem]">
+              <path 
+              strokeLinecap="round" strokeLinejoin="round" 
+              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+              </svg>
           }>
-            <p className="pt-2">info@test.com</p>
+            <p className="pt-[0.6rem]">info@test.com</p>
           </Button>
-      <nav>
+      <nav className="flex flex-col gap-8 mt-16">
         {navItems.map(({ href, label}) => (
           <a href={href} 
              key={label}>
-             {label}
+             <Button variant="text"
+             className="text-lg">{label}</Button>
             </a>
         ))}
       </nav>
       </div>
-      <p className="py-16">Copyright &copy; GoCenter &bull; All rights reserved</p>
+      <p className="py-16 text-white/30 text-sm">Copyright &copy; GoCenter &bull; All rights reserved</p>
     </div>
   </footer>
 };
