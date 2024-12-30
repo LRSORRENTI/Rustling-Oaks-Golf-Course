8f60c7902bf1e22cc2fcecfadd46d879788a8686
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-12-30 09:32:45 -0600

    added setup for footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Intro from "@/sections/Intro";
 import Projects from "@/sections/Projects";
 import Testimonials from "@/sections/Testimonials";
 import FAQs from "@/sections/FAQs";
+import Footer from "@/sections/Footer";
 
 export default function Home() {
   return <>
@@ -13,5 +14,6 @@ export default function Home() {
     <Projects/>
     <Testimonials/>
     <FAQs/>
+    <Footer/>
   </>
 }
