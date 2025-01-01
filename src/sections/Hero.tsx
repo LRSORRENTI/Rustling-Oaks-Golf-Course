dc04435b53cc9b773e2ee5a889f868dd16207542
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-01 08:12:54 -0600

    added more to hero content animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,17 +1,46 @@
-import { FC } from "react";
+'use client'
+
+import { FC, useEffect } from "react";
 import Image from "next/image";
 import Button from "@/components/Button";
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 import heroImage from "@/assets/images/hero-image.jpg";
 
+import SplitType from "split-type";
+import { useAnimate, motion, stagger} from "motion/react";
+
+
 const Hero: FC = () => {
+  const [titleScope, titleAnimate] = useAnimate();
+
+  useEffect(() => {
+      new SplitType(titleScope.current, {
+        types: "lines,words",
+        tagName: "span",
+      });
+
+      titleAnimate(
+        titleScope.current.querySelectorAll('.word'),
+        {
+        transform: 'translateY(0)',
+        }, 
+        {
+         duration: 0.5,
+         delay: stagger(0.2)
+        });
+  }, []);
+
   return <section>
     <div className="grid md:grid-cols-12 md:h-screen items-stretch">
       <div className="md:col-span-7 flex flex-col justify-center">
       <div className="container !max-w-full ">
-          <h1 className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0">
+          <motion.h1 
+          initial={{ opacity: 0}}
+          animate={{ opacity: 1}}
+          ref={titleScope}
+          className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0">
             Crafting digital experiences through code and creative design
-          </h1>
+          </motion.h1>
           <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
           <Button variant="secondary" iconAfter={
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mt-[0.56rem]">
