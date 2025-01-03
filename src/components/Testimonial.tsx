a19916d19e391652c4a1018144287b8ddad1b506
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-02 18:06:10 -0600

    Added custom hook text reveal animation

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,8 +1,13 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useEffect } from "react";
 
 import { twMerge } from "tailwind-merge";
+
+import SplitType from "split-type";
+
+import { useAnimate } from "motion/react";
+import useTextRevealAnimation from "@/hooks/useTextRevealAnimation";
 
 const Testimonial = (props: {
     quote: string;
@@ -16,6 +21,12 @@ const Testimonial = (props: {
 
 const {quote, name, role, company, image, imagePositionY, className, ...rest} = props;
 
+const { scope: quoteScope, entranceAnimation: quoteAnimate } = useTextRevealAnimation();
+
+const [citeScope, citeAnimate] = useAnimate();
+
+
+
 return (
           <div  className={twMerge("grid md:grid-cols-5 md:gap-8 lg:gap-16 md:items-center", className)}
           {...rest}>
@@ -23,11 +34,9 @@ return (
                       <Image src={image} alt={name} className="object-cover size-full" style={{objectPosition: `50% ${imagePositionY * 100}%`}}/>
                     </div>
                     <blockquote className="md:col-span-3">
-                      <div className="text-3xl md:text-5xl lg:text-6xl mt-8 md:mt-0">
+                      <div className="text-3xl md:text-5xl lg:text-6xl mt-8 md:mt-0" ref={quoteScope}>
                     <span>&ldquo;</span>
-                      <span>
                         {quote}
-                      </span>
                       <span>&rdquo;</span>
                       </div>
                       <cite className="mt-4 md:mt-8 not-italic block md:text-lg lg:text-xl">{name}, {role} at {company}</cite>
