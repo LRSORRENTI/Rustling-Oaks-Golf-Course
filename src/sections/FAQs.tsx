0faa73cea507c6a3b53d3cc7e789c8ec35c318d3
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-03 13:41:35 -0600

    Added more to faq hover animations

--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -38,18 +38,27 @@ const FAQs: FC = () => {
           <h2 className="text-4xl md:text-7xl lg:text-8xl ">FAQs</h2>
           <div className="mt-10 md:mt-16 lg:mt-20">
             {faqs.map(({question, answer}, faqIndex ) => (
-              <div key={question} className="border-t border-stone-400 border-dotted last:border-b py-6 md:py-8 lg:py-10"
+              <div key={question} className="border-t border-stone-400 border-dotted last:border-b py-6 md:py-8 lg:py-10 relative isolate group/faq"
                   onClick={() => {
                     if (faqIndex === selecedIndex) {
                       setSelectedIndex(null);
                     } else  {
                       setSelectedIndex(faqIndex)}
                       }}>
-                <div className="flex items-center justify-between gap-4">
+                        <div
+                          className={twMerge(`
+                          absolute h-0 w-full
+                          bottom-0 left-0
+                          bg-stone-300 -z-10 group-hover/faq:h-full
+                          transition-all duration-700
+                        `, faqIndex === selecedIndex ? "h-full" : "")}>
+
+                        </div>
+                <div className={twMerge(`flex items-center justify-between gap-4 transition-all duration-700`, faqIndex === selecedIndex && 'lg:px-8')}>
                   <div className="text-2xl md:text-3xl lg:text-4xl">
                     {question}
                   </div>
-                      <div className={twMerge("inline-flex items-center justify-center  size-11 border border-stone-400 rounded-full shrink-0 transition duration-400", faqIndex === selecedIndex ? " rotate-45" : "")}>
+                      <div className={twMerge ("inline-flex items-center justify-center size-11 border border-stone-400 rounded-full shrink-0 transition duration-400 bg-stone-200", faqIndex === selecedIndex ? "  rotate-45" : "")}>
                           <svg 
                           xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -59,7 +68,7 @@ const FAQs: FC = () => {
                 <AnimatePresence>
                 {faqIndex === selecedIndex && (
                     <motion.div
-                      className="overflow-hidden"
+                      className="overflow-hidden lg:px-8"
                       initial={{height: 0}}
                       animate={{height: 'auto'}}
                       exit={{height: 0}}
