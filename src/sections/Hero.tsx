2bfecccb8ff6a1a9a4f4fc3391a43e7174d724b8
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-03 15:07:05 -0600

    Added working links to footer nav links

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -31,7 +31,7 @@ const Hero: FC = () => {
 
   return (
   <section>
-    <div className="grid md:grid-cols-12 md:h-screen items-stretch sticky top-0 ">
+    <div className="grid md:grid-cols-12 md:h-screen items-stretch sticky top-0 " id="about">
       <div className="md:col-span-7 flex flex-col justify-center">
       <div className="container !max-w-full ">
           <motion.h1 
