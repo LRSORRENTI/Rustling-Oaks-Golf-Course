e95ece5a9eae31fc8ebab6a4f786d776feec33ab
Luke Sorrenti <lrsorrenti@hotmail.com>
2025-01-11 08:47:01 -0600

    added more default styles for the explore section slug pages

--- src/app/explore/[slug]/page.tsx
+++ src/app/explore/[slug]/page.tsx
@@ -12,6 +12,7 @@ import { FC, useEffect, useRef } from "react";
 
 import {  motion, useScroll, useTransform} from "motion/react";
 import useTextRevealAnimation from "@/hooks/useTextRevealAnimation";
+import { div } from "motion/react-client";
 
 
 
@@ -32,15 +33,15 @@ export default function CourseDetails({ params }: any) {
 
 
   return (
-    <section className="section">
-      <div className="container">
+      <div className="">
+      <div className="border border-green-500 flex flex-col lg:flex-row lg:mt-4 mx-auto">
         {/* Animated H1 */}
         <motion.h1
            initial={{ opacity: 0}}
            animate={{ opacity: 1}}
         //   animate={controls}
           ref={scope}
-          className="text-5xl md:text-8xl lg:text-9xl mb-6"
+          className="text-5xl md:text-8xl lg:text-9xl mb-6 lg:pr-28 lg:ml-24 lg:pt-20"
         >
           {project.name}
         </motion.h1>
@@ -48,17 +49,17 @@ export default function CourseDetails({ params }: any) {
           <Image
             src={project.image}
             alt={`${project.name} image`}
-            className="rounded-lg"
+            className="rounded-lg lg:pr-4"
           />
         </div>
-        <div className="mt-8 text-lg md:text-xl lg:text-2xl">
+      </div>
+      <div className="mt-8 text-lg md:text-xl lg:text-2xl flex mx-auto justify-center text-left max-w-[46rem]">
           <p>
             Welcome to {project.name}! Here you'll find more details about this
             part of Rustling Oaks. (Add more descriptive text here about the
             course, driving range, mini golf, or restaurant.)
           </p>
         </div>
-      </div>
-    </section>
+   </div>
   );
 }
